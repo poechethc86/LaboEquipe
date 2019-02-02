fbd094be47c336d6dd60a22cf6b7a399f1e0336a
poechethc86 <46837855+poechethc86@users.noreply.github.com>
2019-02-02 11:28:54 +0100

    Maj mda

--- src/be/ifosup/Utils/MD5Manager.java
+++ src/be/ifosup/Utils/MD5Manager.java
@@ -1,4 +1,28 @@
 package be.ifosup.Utils;
 
-public class MD5Manager {
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5Manager {
+
+    public String hash(String pass){
+
+
+        //String mdme ="";
+
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        md.update(pass.getBytes());
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+        //mdme = myHash;
+
+    return myHash;}
+
 }
