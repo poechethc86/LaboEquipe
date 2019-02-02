391983f50bd693470896fcfe6c2b0b7d39cc857b
Sophie Roch <scallakim@hotmail.com>
2019-02-02 11:58:11 +0100

    Switch between login and homepage

--- src/be/ifosup/login/ServletLogin.java
+++ src/be/ifosup/login/ServletLogin.java
@@ -7,13 +7,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ServletLogin", urlPatterns = {"/"})
+@WebServlet(name = "ServletLogin", urlPatterns = {"/login"})
 public class ServletLogin extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // récupération des deux paramètres transmis par le formulaire
+        String nom = request.getParameter("nom");
+        String pass = request.getParameter("pass");
+
+        // test avec la méthode de validation
+        if(nom.equals("test") && pass.equals("pass")) {
+            request.getSession().setAttribute("nom", nom);
+
+            response.sendRedirect("homepage");
+        } else {
+            request.setAttribute("errorMessage", "Login invalide");
+            request.getRequestDispatcher("/vues/login.jsp").forward(request, response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/vues/homepage.jsp").forward(request,response);
+        request.getRequestDispatcher("/vues/login.jsp").forward(request,response);
     }
 }
---
+++ src/be/ifosup/page/ServletHomepage.java
@@ -0,0 +1,19 @@
+package be.ifosup.page;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletHomepage", urlPatterns = {"/homepage"})
+public class ServletHomepage extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/vues/homepage.jsp").forward(request,response);
+    }
+}
