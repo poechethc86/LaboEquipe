9b2089022b9b6531640ce5c5107205068eb9f911
Sophie Roch <scallakim@hotmail.com>
2019-02-02 10:56:49 +0100

    Add login filter

---
+++ src/be/ifosup/filter/LoginRequestFilter.java
@@ -0,0 +1,30 @@
+package be.ifosup.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "*")
+public class LoginRequestFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        if(request.getSession().getAttribute("nom") != null) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            request.getRequestDispatcher("login").forward(servletRequest, servletResponse);
+        }
+        System.out.println(request.getRequestURI());
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/be/ifosup/login/ServletLogin.java
@@ -0,0 +1,19 @@
+package be.ifosup.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletLogin", urlPatterns = {"/"})
+public class ServletLogin extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/vues/homepage.jsp").forward(request,response);
+    }
+}
