2e7cf7ea410725e8c9b9011bc8d1f9ed86ae26d4
JustinGendarme <gendarme.1010.student@ifosupwavre.be>
2019-02-09 12:15:43 +0100

    new club

--- src/be/ifosup/entities/Club.java
+++ src/be/ifosup/entities/Club.java
@@ -5,17 +5,19 @@ public class Club {
     //attributs:
     private String nom;
     private String sport;
+    private int pk_club;
 
     //constructeur:
 
-    public Club(String nom, String sport) {
+    public Club(String nom, String sport, int pk_club) {
         this.nom = nom;
         this.sport = sport;
+        this.pk_club= pk_club;
     }
     public Club(String nom) {
-        this(nom,"");
+        this(nom,"",-1);
     }
-    
+
 
     //getter setter:
 
@@ -33,5 +35,13 @@ public class Club {
 
     public void setSport(String sport) {
         this.sport = sport;
+    }
+
+    public int getPk_club() {
+        return pk_club;
+    }
+
+    public void setPk_club(int pk_club) {
+        this.pk_club = pk_club;
     }
 }
