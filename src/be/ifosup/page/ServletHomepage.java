d015ec7c8deb45944c643ad8ab84545f578204af
JustinGendarme <gendarme.1010.student@ifosupwavre.be>
2019-02-16 11:14:24 +0100

    modif servlet

--- src/be/ifosup/page/ServletHomepage.java
+++ src/be/ifosup/page/ServletHomepage.java
@@ -1,6 +1,8 @@
 package be.ifosup.page;
 
+import be.ifosup.entities.ClubService;
 import be.ifosup.entities.MembreService;
+import be.ifosup.entities.SportService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,13 +14,17 @@ import java.io.IOException;
 @WebServlet(name = "ServletHomepage", urlPatterns = {"/homepage"})
 public class ServletHomepage extends HttpServlet {
     private MembreService membreService = new MembreService();
+    private ClubService clubService = new ClubService();
+    private SportService sportService = new SportService();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("membres",membreService.recupereMembre());
-        System.out.println(request.getAttribute("membres"));
+        request.setAttribute("clubs",clubService.recupereClub());
+        request.setAttribute("sports",sportService.recupereSport());
         request.getRequestDispatcher("/vues/homepage.jsp").forward(request,response);
     }
 }
