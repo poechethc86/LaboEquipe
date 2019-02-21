cdd4a2ebf33e36ca36f3ea8a166236f33d378e9d
Julien <julien.huyghe@yahoo.fr>
2019-02-21 19:35:06 +0100

    bouton suppression clud dans page infoclub

---
+++ src/be/ifosup/page/Servletdeleteclub2.java
@@ -0,0 +1,28 @@
+package be.ifosup.page;
+
+import be.ifosup.database.ClubManager;
+import be.ifosup.entities.Club;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servletdeleteclub2",urlPatterns = "/delete-clubs")
+public class Servletdeleteclub2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        Club club = new Club(Integer.parseInt(request.getParameter("clubId")));
+        ClubManager.Instance().RemoveClub(club);
+
+        response.sendRedirect("/homepage");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
